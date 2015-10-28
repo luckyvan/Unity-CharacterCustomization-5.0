fb69ba6c62ec6dd2f9893c5e39111884d0842c74
Fan Chou <Chou.Fan@igt.com>
2015-10-28 11:03:44 +0800

    9.2 how to create/load bundle. The Client.cs should be attached to a GameObject to start.

---
+++ Assets/_Scripts/Client.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+	IEnumerator Start () {
+		string bundlePath = "file://" + Application.dataPath + "/MyData.bundle";
+		WWW www = WWW.LoadFromCacheOrDownload (bundlePath, 1);
+		yield return www;
+
+		if (!string.IsNullOrEmpty (www.error)) {
+			print (www.error);
+			yield return null;
+		}
+
+		MyData data = www.assetBundle.mainAsset as MyData;
+		if (data != null) {
+			print (data.content[0]);
+			print (data.content[1]);
+			print (data.content[2]);
+		}
+		yield return null;
+	}
+
+}
---
+++ Assets/_Scripts/MyExporter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class MyExporter : MonoBehaviour {
+	[MenuItem("Assets/MyExporter")]
+	static void MyExec(){
+		// Init Scriptable Object
+		MyData data = ScriptableObject.CreateInstance <MyData>();
+		data.content = new List<Vector3>(){
+			new Vector3(0, 1, 2),
+			new Vector3(3, 4, 5),
+			new Vector3(6, 7, 8)
+		};
+
+		// Create Asset by Scriptable Object
+		string assetPath = "Assets/MyData.asset";
+		AssetDatabase.CreateAsset (data, assetPath);
+
+		// Create bundle by Asset
+		Object asset = AssetDatabase.LoadAssetAtPath (assetPath, typeof(MyData));
+		string bundlePath = "Assets/MyData.bundle";
+		BuildPipeline.BuildAssetBundle (asset, null, bundlePath);
+
+		//Clean
+		AssetDatabase.DeleteAsset (assetPath);
+	}
+}
