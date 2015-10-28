9552241d8a425306cf74adbf668b7f0538d75c0c
Fan Chou <Chou.Fan@igt.com>
2015-10-28 16:16:08 +0800

    9.3.2 Create BaseAssetBundle. in Editor Mode, can use DestroyImmediate only.

--- Assets/Plugins/Editor/CreateAssetBundle.cs
+++ Assets/Plugins/Editor/CreateAssetBundle.cs
@@ -21,8 +21,16 @@ public class CreateAssetBundle{
 			if (!AssetDatabase.GetAssetPath (obj).Contains ("/characters/")) {
 				continue;
 			}
-			Debug.Log (AssetDatabase.GetAssetPath (obj));
-			createBundle = true;
+			//deal with model files only
+
+			if (AssetDatabase.GetAssetPath (obj).Contains ("FBX")) {
+				Debug.Log (AssetDatabase.GetAssetPath (obj));
+
+				GameObject fbx = (GameObject)obj;
+				string name = fbx.name.ToLower ();
+				CreateCharacterBaseAssetBundle (fbx, name);
+				createBundle = true;				
+			}
 		}
 
 		if (!createBundle) {
@@ -31,5 +39,40 @@ public class CreateAssetBundle{
 				"Can not generate AssetBundle. Please select \"Character\" folder or its subfolder in \"Project\" view to generate bundle", "OK");
 			return;
 		}
+	}
+
+	static void CreateCharacterBaseAssetBundle (GameObject fbx, string name)
+	{
+		if (!Directory.Exists (AssetBundlePath)) {
+			Directory.CreateDirectory (AssetBundlePath);
+		}
+
+		//delete bundle with the same name
+		string bundleName = name + "_characterbase.assetbundle";
+		string[] existingAssetBundles = Directory.GetFiles (AssetBundlePath);
+		foreach (var bundle in existingAssetBundles) {
+			if (bundle.EndsWith (bundleName)) {
+				File.Delete (bundle);
+			}
+		}
+
+		GameObject clone = (GameObject)Object.Instantiate (fbx);
+		foreach (var animation in clone.GetComponentsInChildren<Animation>()) {
+			animation.cullingType = AnimationCullingType.AlwaysAnimate;
+		}
+		foreach (var skinedMeshRender in clone.GetComponentsInChildren<SkinnedMeshRenderer>()) {
+			Object.DestroyImmediate(skinedMeshRender.gameObject);
+		}
+		clone.AddComponent<SkinnedMeshRenderer>();
+
+		//Prefab
+		string prefabPath = "Assets/" + name + ".prefab";
+		Object tempPrefab = PrefabUtility.CreateEmptyPrefab (prefabPath);
+		tempPrefab = PrefabUtility.ReplacePrefab (clone, tempPrefab);
+		// clean cloned ojbect, In Editor mode, can use DestroyImmediate only.
+		Object.DestroyImmediate (clone);
+
+		BuildPipeline.BuildAssetBundle (tempPrefab, null, AssetBundlePath+bundleName, BuildAssetBundleOptions.CollectDependencies);//, BuildAssetBundleOptions.CollectDependencies is always enabled in 5.0
+		AssetDatabase.DeleteAsset (AssetDatabase.GetAssetPath (tempPrefab));//clean temp prefab file, why not use File.delete????s
 	}
 }
